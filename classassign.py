acf46a38c9208e8f4a3145c4ccb7910b8de76cfc
dikshya <dikshyabhusal48@gmail.com>
2024-08-08 14:34:45 +0545

    assignment

--- classassign.py
+++ classassign.py
@@ -1,4 +1,13 @@
 #WAP which take input as age and print they are old or not
-name=input("Enter your name")
-age=input("Enter your age")
-print(f"your name is {name} and age is {age}")
+x=input("Enter your age")
+
+if (x>50) :
+    print("you are old")
+    if x<49:
+        print("You are not old.Enjoy your day")
+    else:
+        print("you are kid.Live your life with more fun")
+
+
+
+
--- string.py
+++ string.py
@@ -1,29 +1,29 @@
 name = "ram"
-print(name.title())         #title
-print(name.swapcase())      #swapcase
+print(name.title())         #title(It print first lesson capital and other small as a title)
+print(name.swapcase())      #swapcase(swap the lowercase to uppercase and viceversa)
 
-text= "Hello Friend"
+text= "hello Friend"
 
-print(text.upper())         #upper
-print(text.capitalize())    #capitalize
-print(text.casefold())      #casefold
-print(text.isprintable())   #isprintable
+print(text.upper())         #upper (Convert text into uppercase)
+print(text.capitalize())    #capitalize(same as title does)
+print(text.casefold())      #casefold (convert to lowercase)
+print(text.isprintable())   #isprintable (returns true if strings are printable)
 
-#join
+#join (join the letters with any operator indicated below)
 girls=("isha","anusa","dikshya")
 x="#".join(girls)
 print(x)
 
-#count
+#count(count the number of strings present there)
 txt="I love coding,coding is my favorite"
 y=txt.count("coding")
 print(y)
 
-#split
+#split(split the string and gives a list)
 print(txt.split())
 
 
-#strip
+#strip (rstrip for)
 abc="   hello   "
 z=abc.rstrip()
 print("everyone say",z,"To my brother")
