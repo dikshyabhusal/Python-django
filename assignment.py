b9fcdb62851fc0bb39820490f734d0e717812a5c
dikshya <dikshyabhusal48@gmail.com>
2024-08-08 22:31:05 +0545

    Validateemail

--- assignment.py
+++ assignment.py
@@ -1,11 +1,24 @@
 #Wap to take user input email and validate wheather it is corrent email or not
 
+email = input("Enter your email: ")
 
+def validate_email(email):
+    if email.count('@') != 1:
+        return False
     
-email = "abc@gmailcom"
+    first_part, domain_part = email.split('@')
 
-if '@' in email and '.com' in email:
-    print("this is correct email")
+    if domain_part.count('.') != 1:
+        return False
+    
+    domain_name, extension = domain_part.split('.')
+    
+    if not (first_part.isalpha() and domain_name.isalpha() and extension.isalpha()):
+        return False
+    
+    return True
 
+if validate_email(email):
+    print("The email is valid.")
 else:
-    print("this is wrong email")
+    print("The email is not valid. It should not contain any numbers or special characters.")
